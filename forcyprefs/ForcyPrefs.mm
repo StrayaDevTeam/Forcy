0a955542d5024ff6abecaf5344659b383bacf3d0
GreenyDev <greenydev@greenmail.net.au>
2015-10-17 18:36:17 +1030

    removed that

--- forcyprefs/ForcyPrefs.mm
+++ forcyprefs/ForcyPrefs.mm
@@ -13,18 +13,6 @@ int width = self.view.bounds.size.width;
 	}
 	return _specifiers;
 }
-
-//who fucking knows, enjoy doing this shit hayden
-- (void)viewWillAppear:(BOOL)arg1{
-	[super viewWillAppear:arg1];
-
-	UIImage *banner = [[UIImage alloc] initWithContentsOfFile:@"/Library/PreferenceBundles/ForcyPrefs.bundle/Banner.png"];
-	UIImageView *bannerView = [[UIImageView alloc] initWithImage:banner];
-	[bannerView setFrame:CGRectMake(0,0,[self.view].bounds.size.width,100)];
-	bannerView.contentMode = UIViewContentModeScaleAspectFit;
-
-	[self.view addSubview:bannerView];
-}
 @end
 
 // vim:ft=objc
